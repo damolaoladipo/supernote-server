494fdcfd1aa729ae70c985f293e0ec6766ff0162
Damola <odamola@mit.edu>
2024-10-24 15:28:26 +0100

    added configurations

---
+++ src/config/app.config.ts
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+import express, { Request, Response, NextFunction } from "express";
+
+
+config()
+
+const app = express()
+
+
+export default app
---
+++ src/server.ts
@@ -0,0 +1,3 @@
+import app from "./config/app.config";
+import colors from "colors";
+import connectDB from "./config/db.config";
